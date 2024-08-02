c80081424da2b9ddc7102ae1d8ba4e46c9eccd2b
Rafhael <janke.irineu@gmail.com>
2024-08-02 19:12:45 -0300

    upado pasta vendor

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -5,23 +5,18 @@ set -e
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html/ /var/www/html/storage /var/www/html/bootstrap/cache
 
-# Criar a pasta vendor se não existir
+# Instalar dependências do Laravel se a pasta vendor não existir
 if [ ! -d "/var/www/html/vendor" ]; then
-    mkdir -p /var/www/html/vendor
-    chown -R www-data:www-data /var/www/html/vendor
-fi
-
-# Instalar dependências do Laravel se a pasta vendor estiver vazia
-if [ ! "$(ls -A /var/www/html/vendor)" ]; then
     echo "Instalando dependências do Composer..."
+    composer clear-cache
+
     composer install --no-dev --optimize-autoloader --prefer-dist
 fi
 
-# Copiar .env.example para .env se .env não existir
-if [ ! -f "/var/www/html/.env" ]; then
+# Gerar a chave da aplicação Laravel
+if [ ! -f ".env" ]; then
     echo "Copiando .env.example para .env..."
-    cp /var/www/html/.env.example /var/www/html/.env
-    chown www-data:www-data /var/www/html/.env
+    cp .env.example .env
 fi
 
 echo "Limpando e gerando caches..."
@@ -36,7 +31,7 @@ php artisan vendor:publish --provider="Maatwebsite\Excel\ExcelServiceProvider"
 echo "Gerando autoload do Composer..."
 composer dump-autoload
 
-# Executar migrações do Laravel, se necessário
+# Executar migrações do Laravel, se necessárias
 if [ "$RUN_MIGRATIONS" = "true" ]; then
     echo "Executando migrações do banco de dados..."
     php artisan migrate --force
